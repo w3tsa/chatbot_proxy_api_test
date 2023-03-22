1c9efe46ed03a269c9d6117bd30bbcb9f7575ae5
Tanveer Sayem <tanveersayem@gmail.com>
2023-03-22 08:59:42 -0400

    Added the post request

--- src/api/hello.js
+++ src/api/hello.js
@@ -3,7 +3,24 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: 'Hello World!' });
+});
+
+router.post('/', (req, res) => {
+  const { message } = req.body;
+  let response = '';
+  switch (message.toLowerCase().trim()) {
+    case 'hi' || 'hello':
+      response = 'Hi There!';
+      break;
+    case 'how are you?':
+      response = 'I am fine, Thank you for asking, how are you?';
+      break;
+    default:
+      response = 'Hello, world!';
+  }
+  res.status(201).json({ message: response });
 });
 
 module.exports = router;
